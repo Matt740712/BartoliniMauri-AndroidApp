9053b1fc91f2cb5ee7ddffc93b85398623c64234
Riccumeni <77925342+Riccumeni@users.noreply.github.com>
2023-06-22 15:10:48 +0200

    login

--- app/src/main/java/com/example/bartolini_mauri_login/APIService.kt
+++ app/src/main/java/com/example/bartolini_mauri_login/APIService.kt
@@ -1,14 +1,15 @@
-package com.example.bartolini_mauri_login
-
-import retrofit2.http.Headers
+import com.example.bartolini_mauri_login.models.LoginResponse
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface APIService {
-    /*
-  @POST ("login")
-  @Headers("Accept:application/json", "content-type:application/jason",
-     "Authorization: Bearer " )
-
-*/
+    @FormUrlEncoded
+    @POST("Login")
+    fun login(
+        @Field("username") username : String,
+        @Field("password") password : String
+    ) : Call<LoginResponse>
 }
-
---
+++ app/src/main/java/com/example/bartolini_mauri_login/models/customer/Company.kt
@@ -0,0 +1,9 @@
+package com.example.bartolini_mauri_login.models.customer
+
+data class Company(
+    val address: String,
+    val email: String,
+    val name: String,
+    val telephone: String,
+    val webSite: String
+)
