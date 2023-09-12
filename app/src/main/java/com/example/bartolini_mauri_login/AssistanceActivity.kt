afcec0480567789b0ac18decbee9854f8caa014a
Matt740712 <mattiatar7@gmail.com>
2023-09-12 17:06:06 +0200

    Inizio Page Assistance

---
+++ app/src/main/java/com/example/bartolini_mauri_login/AssistanceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bartolini_mauri_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AssistanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_assistance)
+    }
+}
