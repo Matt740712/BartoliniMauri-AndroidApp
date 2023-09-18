b0cd0d841c56e365321bb1f63c401b8f6d9115a2
Matt740712 <mattiatar7@gmail.com>
2023-09-18 17:21:48 +0200

    Assistenza

---
+++ app/src/main/java/com/example/bartolini_mauri_login/AssistanceActivity.kt
@@ -0,0 +1,24 @@
+package com.example.bartolini_mauri_login
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class AssistanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_assistance)
+
+
+//cambiare metodo di slide pagina
+
+        @SuppressLint("MissingPermission")
+        fun onLayoutAssistanceClick(view: View) {
+            // Avvia l'Activity AssistanceActivity
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
